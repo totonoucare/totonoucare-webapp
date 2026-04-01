348e3638b22a827a2cff758213d62cc7c28c848f
totonoucare <novelinks9@gmail.com>
2026-04-01 16:57:14 +0900

    Create index.js

---
+++ components/illust/home/index.js
@@ -0,0 +1,4 @@
+export { default as HeroMain } from "@/components/illust/home/HeroMain";
+export { default as HeroScene } from "@/components/illust/home/HeroScene";
+export { default as HeroGuideBot } from "@/components/illust/home/HeroGuideBot";
+export { HeroRiskCard, HeroCareCard } from "@/components/illust/home/HeroMiniCards";
