43684683ce2af71de6eb9ef93cdb2ec4fb7c994e
totonoucare <novelinks9@gmail.com>
2026-04-02 03:08:10 +0900

    Update index.js

--- components/illust/home/index.js
+++ components/illust/home/index.js
@@ -2,3 +2,5 @@ export { default as HeroMain } from "./HeroMain";
 export { default as HeroScene } from "./HeroScene";
 export { default as HeroGuideBot } from "./HeroGuideBot";
 export * from "./HeroMiniCards";
+
+export { default as HeroTitleMark } from "./HeroTitleMark";
