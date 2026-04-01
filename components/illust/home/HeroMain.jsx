27b1965659d92aafa3243f5736416bdede645851
totonoucare <novelinks9@gmail.com>
2026-04-01 17:56:51 +0900

    Update HeroMain.jsx

--- components/illust/home/HeroMain.jsx
+++ components/illust/home/HeroMain.jsx
@@ -5,18 +5,18 @@ import { HeroRiskCard, HeroCareCard } from "@/components/illust/home/HeroMiniCar
 export default function HeroMain() {
   return (
     <div className="relative mx-auto w-full max-w-[360px]">
-      <HeroScene className="h-[320px] w-full" />
+      <HeroScene className="h-[324px] w-full" />
 
-      <div className="absolute left-3 top-4 w-[212px] sm:left-2 sm:w-[224px]">
+      <div className="absolute left-3 top-4 w-[220px] sm:w-[230px]">
         <HeroRiskCard />
       </div>
 
-      <div className="absolute right-3 top-[144px] w-[176px] sm:w-[188px]">
+      <div className="absolute right-3 top-[150px] w-[188px] sm:w-[194px]">
         <HeroCareCard />
       </div>
 
-      <div className="absolute bottom-2 right-2">
-        <HeroGuideBot className="h-[122px] w-[104px]" />
+      <div className="absolute bottom-1 right-1">
+        <HeroGuideBot className="h-[138px] w-[118px]" />
       </div>
     </div>
   );
