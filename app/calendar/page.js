d9e2be3f4451ea3ab121c68911b455d33c730e92
totonoucare <novelinks9@gmail.com>
2026-07-27 15:51:48 +0900

    Delete app/calendar directory

--- app/calendar/page.js
+++
@@ -1,8 +0,0 @@
-import RecordsPageClient from "@/components/records/RecordsPageClient";
-
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
-export default function CalendarPage() {
-  return <RecordsPageClient initialTab="record" />;
-}
