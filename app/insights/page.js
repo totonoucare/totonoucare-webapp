3e44e85787bd8ecea138bb01d91fed1c65cacc6f
totonoucare <novelinks9@gmail.com>
2026-07-27 15:52:05 +0900

    Delete app/insights directory

--- app/insights/page.js
+++
@@ -1,8 +0,0 @@
-import RecordsPageClient from "@/components/records/RecordsPageClient";
-
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
-export default function InsightsPage() {
-  return <RecordsPageClient initialTab="analysis" />;
-}
